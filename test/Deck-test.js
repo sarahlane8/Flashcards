cf55c06a8e3bb4610b0ae4b38add76b20a684966
Sarah Lane <sarahlane8@gmail.com>
2021-03-22 17:12:46 -0600

    Add Deck class skeleton and test

---
+++ test/Deck-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck.js');
+// const Card = require('../src/Card.js');
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+  })
+})
