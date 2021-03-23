f3148e2b6643f0fc1dc53c91ce7bc33cc278d525
Sarah Lane <sarahlane8@gmail.com>
2021-03-23 07:19:53 -0600

    Refactor test file with beforeEach

--- src/Round.js
+++ src/Round.js
@@ -1,12 +1,23 @@
 class Round {
   constructor(deck) {
-    this.currentCard = deck[0];
+    this.deck = deck;
+    this.currentCard = {};
+    this.turns = 0;
   }
 
   returnCurrentCard() {
+    this.currentCard = this.deck[0];
     return this.currentCard;
   }
-};
 
+  takeTurn() {
+    this.turns++;
+    //when a guess is made, a new Turn instance is created
+    // this.currentCard = this.deck[i++];
+  }
+};
+//evaluate guesses
+//give feedback
+//stores ids of incorrect guesses
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -6,19 +6,43 @@ const Deck = require('../src/Deck.js');
 const Card = require('../src/Card.js');
 
 describe('Round', function() {
-  it('should have a property of currentCard', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const deck = new Deck(card);
-    const round = new Round(deck);
-    expect(round.currentCard).to.equal(deck[0]);
+  let card1
+  let card2
+  let card3
+  let cards
+  let deck
+  let round
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separate listed of related values?', ['array', 'object', 'function'], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    cards = [card1, card2, card3];
+    deck = new Deck(cards);
+    round = new Round(deck);
   });
 
+  it('should have a property of currentCard', function() {
+    expect(round.currentCard).to.deep.equal({});
+  });
+
+
   it('should return the current card', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const deck = new Deck(card);
-    const round = new Round(deck);
-    expect(round.returnCurrentCard()).to.equal(round.currentCard);
+    expect(round.returnCurrentCard()).to.deep.equal(round.currentCard);
   })
 
-  
+
+  it.skip('should have a takeTurn method that updates the turns count', function() {
+    expect(round.turns).to.equal(0);
+    round.takeTurn();
+    expect(round.turns).to.equal(1);
+  })
+
+  //test for making a new Turn instance when a guess is made
+
+  it('should have a takeTurn method that change the current card to the next card in the deck', function() {
+    expect(round.currentCard).to.deep.equal(deck[0]);
+    round.takeTurn();
+    expect(round.currentCard).to.deep.equal(deck[1]);
+  })
 });
