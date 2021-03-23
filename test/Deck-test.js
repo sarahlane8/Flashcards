cc12d72a88fa31504b43daeff91e64fd0a16494b
Sarah Lane <sarahlane8@gmail.com>
2021-03-22 18:16:18 -0600

    Add minor change from eslint

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -7,7 +7,6 @@ const Card = require('../src/Card.js');
 describe('Deck', function() {
 
   it('should be a function', function() {
-    const deck = new Deck();
   })
 
   it('should be instantiated with cards', function() {
